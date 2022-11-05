29399a3e1adca7fe548ccd05144cdfdc2cf8f1c4
FranChesKo <khl_doss@mail.ru>
2022-11-06 02:44:37 +0800

    make the same dynamic height for all boxes in Services section

--- js/main.js
+++ js/main.js
@@ -130,3 +130,13 @@
 
 })(jQuery);
 
+// Make same dynamic height for all boxes (used in Services section)
+$(function(){	
+	var column = 0;
+	$('.box').each(function(){
+		h = $(this).height();
+		if (h > column) {
+			column = h;
+		}
+	}).height(column);
+});
